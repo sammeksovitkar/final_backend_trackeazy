ddd2cd5f03626d0317356784ef65cdb6d1cf4d53
sammeksovitkar <83156211+sammeksovitkar@users.noreply.github.com>
2025-07-29 00:12:51 +0530

    Update Driver.js

--- models/Driver.js
+++ models/Driver.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const driverSchema = new mongoose.Schema({
   driverName: { type: String, required: true },
   vehicleNo: { type: String, required: true, unique: true },
-  vehicleType: { type: String, required: true }, // 🚨 NEW FIELD
-  password: { type: String, required: true },    // Hashed password
+  vehicleType: { type: String, required: true },
+  password: { type: String, required: true },
+  status: {
+    type: Number, // Changed from String to Number
+    enum: [0, 1], // 0 = stop, 1 = running
+    default: 0,
+  },
 });
+
+
+
 
 module.exports = mongoose.model('Driver', driverSchema);
