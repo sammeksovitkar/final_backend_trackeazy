c35836877c55606b9d954ee63e1bea748ac0d4e3
sammeksovitkar <83156211+sammeksovitkar@users.noreply.github.com>
2025-07-29 17:11:03 +0530

    Update Driver.js

--- models/Driver.js
+++ models/Driver.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose');
 
 const driverSchema = new mongoose.Schema({
-  driverName: { type: String, required: true },
-  vehicleNo: { type: String, required: true, unique: true },
-  vehicleType: { type: String, required: true },
-  password: { type: String, required: true },
-  status: {
-    type: Number, // Changed from String to Number
-    enum: [0, 1], // 0 = stop, 1 = running
-    default: 0,
-  },
-});
-
-
-
+  driverName: String,
+  vehicleNo: { type: String, unique: true },
+  vehicleType: String,
+  password: String,
+  status: { type: Number, default: 0 },
+  allocatedTrip: {
+    source: {
+      lat: Number,
+      long: Number,
+    },
+    destination: {
+      lat: Number,
+      long: Number,
+    },
+    start: {
+      date: String,
+      time: String,
+    },
+    end: {
+      date: String,
+      time: String,
+    },
+    time: Number // in hours
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Driver', driverSchema);
